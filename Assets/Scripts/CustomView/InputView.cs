4db5eaadccf0f0b3e824bab8425ddcc37cc281e1
cong91 <thanhcong191@gmail.com>
2014-08-17 23:40:21 +0700

    Create Login scenes

---
+++ Assets/Scripts/CustomView/InputView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputView: MonoBehaviour {
+	public void onBlankInput(){	
+		UILabel label = gameObject.GetComponentInChildren<UILabel> ();
+		if (label !=null && !string.IsNullOrEmpty(label.text)) {
+			gameObject.GetComponent<UIInput>().value = "";
+		}
+	}
+	public void onValueChange(){
+		UISprite btnClose = gameObject.GetComponentInChildren<UIAnchor>().transform.GetChild(0).GetComponent<UISprite>();
+		if (btnClose != null) {
+			btnClose.gameObject.SetActive(!string.IsNullOrEmpty(gameObject.GetComponent<UIInput>().value));
+			gameObject.GetComponent<UIInput>().isSelected = true;
+		}
+	}
+}
