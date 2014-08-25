f6b5c74c0eebba7a0ad1f9f30d82a15728cabdaa
cong91 <thanhcong191@gmail.com>
2014-08-26 00:39:53 +0700

    Update type view 1 in lobby screen

--- Assets/Scripts/Lobby/LobbyRowType1.cs
+++ Assets/Scripts/Lobby/LobbyRowType1.cs
@@ -17,8 +17,13 @@ public class LobbyRowType1 : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		float value = Mathf.Lerp(0.731f, 1f, 0.02f / Vector3.SqrMagnitude(transform.position - LobbyScene.centerObject));
+		float value = Mathf.Lerp(0.731f, 1.15f, 0.02f / Vector3.SqrMagnitude(transform.position - LobbyScene.centerObject));
 		gameObject.transform.localScale = new Vector3(value, value, 1f);
+		if (!gameObject.transform.name.Equals (NGUITools.FindInParents<UICenterOnChild> (gameObject).centeredObject.transform.name)) {
+						gameObject.GetComponent<UISprite> ().color = new Color (69f/255f, 69f/255f, 69f/255f);
+		} else {
+			gameObject.GetComponent<UISprite> ().color = new Color (1f, 1f, 1f);
+		}
 	}
 }
 
--- Assets/Scripts/Lobby/LobbyScene.cs
+++ Assets/Scripts/Lobby/LobbyScene.cs
@@ -6,16 +6,15 @@ public class LobbyScene : MonoBehaviour {
 	public UITable tableType1;
 	public UIGrid gridType2;
 	public static Vector3 centerObject = Vector3.one;
-
 	void Start () {
-		tableType1.GetComponent<UIScaleCenterChild> ().onFinished = OnDragFinishGift;
-		tableType1.GetComponent<UIScaleCenterChild> ().CenterOn (tableType1.transform.GetChild (0));
+		tableType1.GetComponent<UICenterOnChild> ().onFinished = OnDragFinishGift;
+		tableType1.GetComponent<UICenterOnChild> ().CenterOn (tableType1.transform.GetChild (0));
 	}
 	
 
 
 	void OnDragFinishGift ()
 	{
-		centerObject = tableType1.GetComponent<UIScaleCenterChild> ().centeredObject.transform.position;
+		centerObject = tableType1.GetComponent<UICenterOnChild> ().centeredObject.transform.position;
 	}
 }
