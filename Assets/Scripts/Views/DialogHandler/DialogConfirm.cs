b729ba89c9d8535d57299e7dbdd77fa59a58b726
cong91 <thanhcong191@gmail.com>
2014-09-25 02:11:41 +0700

    Xong cac su kien cho dialog confirm

---
+++ Assets/Scripts/Views/DialogHandler/DialogConfirm.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void ConfirmClick();
+public delegate void CancelClick();
+public class DialogConfirm : MonoBehaviour
+{
+
+    #region UnityEditor
+    public UILabel title, message;
+    public GameObject btnConfirm, btnCancel, btnClose;
+    #endregion
+    private event ConfirmClick OnClickConfirmListener;
+    private event CancelClick OnClickCancelListener;
+
+    public void SetOnClickConfirmListener(ConfirmClick OnClickConfirmListener)
+    {
+        this.OnClickConfirmListener = OnClickConfirmListener;
+    }
+    public void SetOnClickCancelListener(CancelClick OnClickCancelListener)
+    {
+        this.OnClickCancelListener = OnClickCancelListener;
+    }
+	void Start () {
+	}
+    
+    void onEnable() {
+        UIEventListener.Get(btnConfirm).onClick += onConfirmClickHandler;
+        UIEventListener.Get(btnCancel).onClick += onCancelClickHandler;
+        UIEventListener.Get(btnClose).onClick += onCloseClickHandler;
+    }
+
+
+
+    void OnDisable() {
+        UIEventListener.Get(btnConfirm).onClick -= onConfirmClickHandler;
+        UIEventListener.Get(btnCancel).onClick -= onCancelClickHandler;
+        UIEventListener.Get(btnClose).onClick -= onCloseClickHandler;
+    }
+
+
+    private void onConfirmClickHandler(GameObject go)
+    {
+        if (OnClickConfirmListener != null)
+            OnClickConfirmListener();
+    }
+    private void onCancelClickHandler(GameObject go)
+    {
+        if (OnClickCancelListener != null)
+            OnClickCancelListener();
+    }
+
+    private void onCloseClickHandler(GameObject go)
+    {
+        GameObject.Destroy(gameObject);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
