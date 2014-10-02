9cdd222bab4a769cfb10c485a88b6ac43e831603
cong91 <thanhcong191@gmail.com>
2014-10-03 02:55:25 +0700

    Commit Gameplay scene

--- Assets/Scripts/Views/SceneHandler/Gamelay/PokerGPSide.cs
+++ Assets/Scripts/Views/SceneHandler/Gamelay/PokerGPSide.cs
@@ -6,4 +6,9 @@ using UnityEngine;
 public class PokerGPSide : MonoBehaviour
 {
     public PokerSide CurrentSide;
+	public GameObject[] positionCardFaceCards;
+	public GameObject[] positionCardBackCards;
+	public GameObject[] positionCardGameEnd;
+	public GameObject positionMoney;
+	public GameObject btnSit;
 }
