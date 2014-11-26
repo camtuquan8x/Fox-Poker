c5d3cdf7bb26dcbdaefa11fdd457f711b1857a16
cong91 <thanhcong191@gmail.com>
2014-11-26 22:26:36 +0700

    Commit code thêm cược

--- Assets/Scripts/Views/SceneHandler/Gamelay/PokerGameplayButtonHandler.cs
+++ Assets/Scripts/Views/SceneHandler/Gamelay/PokerGameplayButtonHandler.cs
@@ -131,16 +131,18 @@ public class PokerGameplayButtonHandler : MonoBehaviour
         {
             double maxOtherMoney = GameObject.FindObjectsOfType<PokerPlayerUI>()
                 .Where<PokerPlayerUI>(p => p.data.userName != PokerObserver.Instance.mainPlayer.userName)
-                .Max<PokerPlayerUI>(p => p.data.GetMoney());
-			double maxBind = GameObject.FindObjectsOfType<PokerPlayerUI>()
-				.Where<PokerPlayerUI>(p => p.data.userName != PokerObserver.Instance.mainPlayer.userName)
-					.Max<PokerPlayerUI>(p => p.data.currentBet);
-            double currentMoney = PokerObserver.Instance.mainPlayer.GetMoney();
-            double maxRaise = currentMoney;
-            if (PokerObserver.Instance.mainPlayer.GetMoney() > maxOtherMoney)
+                .Max<PokerPlayerUI>(p => p.data.GetMoney() + p.data.currentBet);
+//			double maxCanBind = GameObject.FindObjectsOfType<PokerPlayerUI>().Max<PokerPlayerUI>(p => p.data.currentBet + p.data.GetMoney());
+			double maxBinded =  GameObject.FindObjectsOfType<PokerPlayerUI>().Max<PokerPlayerUI>(p => p.data.currentBet);
+			double myMoney = PokerObserver.Instance.mainPlayer.GetMoney() +  PokerObserver.Instance.mainPlayer.currentBet;
+			double maxRaise = myMoney;
+			if(myMoney > maxOtherMoney)
 				maxRaise = maxOtherMoney;
-			if(PokerObserver.Instance.mainPlayer.currentBet != 0 || PokerObserver.Instance.mainPlayer.currentBet > PokerObserver.Instance.gameDetails.customConfiguration.SmallBlind)
-				maxRaise = maxRaise - maxBind;
+				//double maxRaise = maxCanBind - maxBinded;
+//            if (PokerObserver.Instance.mainPlayer.GetMoney() > maxOtherMoney)
+//				maxRaise = maxOtherMoney;
+			maxRaise =  maxRaise - maxBinded;
+
 			bettingDialog = new DialogBetting(PokerObserver.Instance.gameDetails.customConfiguration.SmallBlind, maxRaise,(money) =>
             {
                 Puppet.API.Client.APIPokerGame.PlayRequest(PokerRequestPlay.RAISE, money);
